778424332b4f93ce0b21be2b4da8828c00e2a9c9
JoyceNguyen <jnguyen0581@gmail.com>
2018-10-15 12:33:51 -0500

    changes in app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,14 @@
 
-var dogs = ["German Shepherd", "Labrador Retriever", "Schnauzer"];
+$(document).ready(function(){
+   
 
-function displayDogInfo() {
+var dogs = ["German Shepherd", "Labrador Retriever", "Schnauzer"];
+renderButtons();
+
+$(document).on("click", ".dog-btn", function() {
 
     var dog = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + dog + "api_key=SwA3d1i26PVpc3LKmwuGOXar64zMN5uE&limit=10";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + dog + "&api_key=SwA3d1i26PVpc3LKmwuGOXar64zMN5uE&limit=10";
 
     $.ajax({
         url: queryURL,
@@ -12,36 +16,41 @@ function displayDogInfo() {
     })
         .then(function (response) {
             var results = response.data;
-            $("#gifs-appear-here").empty;
+            $("#gifs-appear-here").empty();
+            console.log (results);
+
 
             for (var i = 0; i < results.length; i++) {
-                var dogDiv = $("<div class='dog'>");
+                var dogDiv = $("<div>");
+                dogDiv.addClass("gifs");
                 var rating = response.Rated;
-                var p = $("<p>").text("Rating: " + rating);
+                var p = $("<p>").text("Rating: " + results[i].rating);
+                $("#dogs-view").append(dogDiv);
+                var dogImage = $("<img>");
+                dogImage.addClass("movement");
+                dogImage.attr("src", results[i].images.fixed_height_still.url);
+                dogImage.attr("data-animate", results[i].images.fixed_height.url);
+                dogImage.attr("data-still", results[i].images.fixed_height_still.url);
+                dogImage.attr("data-state", "still");
+                // dogImage.attr("alt", "dog image");
                 dogDiv.append(p);
                 dogDiv.append(dogImage);
-                $("#dogs-view").append(dogDiv);
-                var dogImage = $("<searchImage>");
-                dogImage.attr("src", results[i].images.original_still.url);
-                dogImage.attr("data-animate", results[i].images.original.url);
-                dogImage.attr("data-still", results[i].images.original_still.url);
-                dogImage.attr("src", imageURL);
-                dogImage.attr("alt", "dog image");
                 $("#gifs-appear-here").prepend(dogDiv);
             }
         });
-};
+});
 
 
-$(document).on("click", "searchImage", function () {
-    var dog = $("dog-input").val();
-    dogs.push(dog);
+$(".movement").on("click", function () {
+    console.log("button works");
+    // var dog = $("dog-input").val();
+    // dogs.push(dog);
     var state = $(this).attr("data-state");
-    if (state == still) {
-        $(this).attr("src", $(this).data("animated"));
-        $(this).attr("data-state", "animated");
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
     } else {
-        $(this).attr("src", $(this).data("still"));
+        $(this).attr("src", $(this).attr("data-still"));
         $(this).attr("data-state", "still");
     }
 });
@@ -51,7 +60,7 @@ function renderButtons() {
     $("#buttons-view").empty();
     for (var i = 0; i < dogs.length; i++) {
         var a = $("<button>");
-        a.addClass("#dog-btn");
+        a.addClass("dog-btn");
         a.attr("data-name", dogs[i]);
         a.text(dogs[i]);
         $("#buttons-view").append(a);
@@ -64,6 +73,5 @@ $("#add-dog").on("click", function (event) {
     dogs.push(dog);
     renderButtons();
 });
+});
 
-$(document).on("click", ".dog-btn", displayDogInfo);
-renderButtons();
